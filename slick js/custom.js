699bb92c7f74003ad27bd88933918fa551596d21
tanvirhossen853 <103171957+tanvirhossen853@users.noreply.github.com>
2022-07-22 02:39:14 +0600

    Wedding_website

---
+++ slick js/custom.js
@@ -0,0 +1,34 @@
+// $(".slider").slick({
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//   adaptiveHeight: true,
+//   arrows: true,
+//   autoplay: false,
+// });
+
+$(".slider").slick({
+  centerMode: true,
+  centerPadding: "60px",
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
